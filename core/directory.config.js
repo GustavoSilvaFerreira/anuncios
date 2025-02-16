204b523fa3e15dcfbc8d400c82f2c0bec25d02c5
GustavoSilvaFerreira <guto7626@hotmail.com>
2025-02-16 19:46:21 -0300

    Ajuste estrutura

--- core/directory.config.js
+++ core/directory.config.js
@@ -7,7 +7,7 @@ const DIR_VIDEOS = join(DIR_FILES, "videos");
 const DIR_TEMP = join(RAIZ, "temp");
 const DIR_TO_POST = join(DIR_TEMP, "toPost");
 const DIR_TO_CREATE = join(DIR_TEMP, "toCreate");
-const FILE_FOR_CREATE = join(DIR_TEMP, 'forCreateAd.txt');
+const FILE_FOR_CREATE = join(DIR_FILES, 'forCreateAd.txt');
 // const DIR_VIDEOS = join(RAIZ, "../../", "Downloads");
 
 module.exports = {
